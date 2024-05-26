bc6f921565e86bfea9835b1e13a31ba7163cbefc
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-05-26 23:54:54 +0900

    Add React Slides

--- contents/marp/scripts/main.sh
+++ contents/marp/scripts/main.sh
@@ -5,10 +5,17 @@ cd $SCRIPT_DIR
 cd ..
 
 
-ls src | grep '.md$' | while read file; do
-  npx @marp-team/marp-cli@latest "src/${file}" --html -o "html/${file%.*}.html"
-done
+npx @marp-team/marp-cli@latest --input-dir src --html --output html
+npx @marp-team/marp-cli@latest --input-dir src --pdf --output pdf
 
 DIST_PATH=../../public/slides
 mkdir -p $DIST_PATH
 cp -r html $DIST_PATH
+
+touch slide-links.md
+rm -rf slide-links.md
+ls src | grep '.md$' | while read file; do
+  FILENAME=${file%.*}
+  A_LINK="<a href=\"/note/slides/html/${FILENAME}.html\" target=\"_blank\">${FILENAME}</a><br />"
+  echo "${A_LINK}" >> slide-links.md
+done
--- contents/marp/slide-links.md
+++ contents/marp/slide-links.md
@@ -1,2 +1,2 @@
-
-<a href="/note/slides/html/2024-05-JavaScripts.html" target="_blank">2024-05-JavaScripts</a>
+<a href="/note/slides/html/2024-05-JavaScripts.html" target="_blank">2024-05-JavaScripts</a><br />
+<a href="/note/slides/html/2024-05-React.html" target="_blank">2024-05-React</a><br />
